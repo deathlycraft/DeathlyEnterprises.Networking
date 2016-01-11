05e4ff171b947ca77a099a9494d98e5f4a87c297
Deathlycraft <deathlycraft-github@deathlyenterprises.com>
2016-01-11 13:19:36 -0600

    Added details to Message object. Working on MessageFactory.

--- DeathlyEnterprises.Networking/Message.cs
+++ DeathlyEnterprises.Networking/Message.cs
@@ -1,13 +1,24 @@
-﻿using DeathlyEnterprises.Core.DependencyInjection;
-
-namespace DeathlyEnterprises.Networking
+﻿namespace DeathlyEnterprises.Networking
 {
-    [Injectable(InjectableLifestyle.Transient)]
     public interface IMessage
     {
+        string MessageType { get; }
+        string MessageContents { get; }
+        int MessageContentSize { get; }
     }
 
     public class Message : IMessage
     {
+        public Message(string messageType, string messageContents, int messageContentSize)
+        {
+            MessageType = messageType;
+            MessageContents = messageContents;
+            MessageContentSize = messageContentSize;
+        }
+
+        public string MessageType { get; }
+        public string MessageContents { get; }
+
+        public int MessageContentSize { get; }
     }
 }
--- DeathlyEnterprises.Networking/MessageFactory.cs
+++ DeathlyEnterprises.Networking/MessageFactory.cs
@@ -1,13 +1,19 @@
-﻿using DeathlyEnterprises.Core.DependencyInjection;
+﻿using System;
+using DeathlyEnterprises.Core.DependencyInjection;
 
 namespace DeathlyEnterprises.Networking
 {
     [Injectable(InjectableLifestyle.Singleton)]
     public interface IMessageFactory
     {
+        IMessage CreateMessage<TMessage>(TMessage message);
     }
 
     public class MessageFactory : IMessageFactory
     {
+        public IMessage CreateMessage<TMessage>(TMessage message)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
