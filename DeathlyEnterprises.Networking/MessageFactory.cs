2e5e6b55d71d2e8373d3c2f50a1b9e679887fff2
Deathlycraft <deathlycraft-github@deathlyenterprises.com>
2016-01-11 11:45:08 -0600

    Added Injectable attribute to IMessageFactory interface.

---
+++ DeathlyEnterprises.Networking/MessageFactory.cs
@@ -0,0 +1,13 @@
+﻿using DeathlyEnterprises.Core.Attributes;
+
+namespace DeathlyEnterprises.Networking
+{
+    [Injectable(InjectableLifestyle.Singleton)]
+    public interface IMessageFactory
+    {
+    }
+
+    public class MessageFactory : IMessageFactory
+    {
+    }
+}
