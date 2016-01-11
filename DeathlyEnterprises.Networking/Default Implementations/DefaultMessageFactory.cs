99e038b547261a48f438f23c29d0f138398ed452
Deathlycraft <deathlycraft-github@deathlyenterprises.com>
2016-01-11 14:03:18 -0600

    Added serializer dependency for DefaultMessageFactory.

--- DeathlyEnterprises.Networking/Default Implementations/DefaultMessageFactory.cs
+++ DeathlyEnterprises.Networking/Default Implementations/DefaultMessageFactory.cs
@@ -1,9 +1,17 @@
 ﻿using System;
+using DeathlyEnterprises.Core.Serialization;
 
 namespace DeathlyEnterprises.Networking
 {
     public class DefaultMessageFactory : IMessageFactory
     {
+        private readonly ISerializer<byte[]> _serializer;
+
+        public DefaultMessageFactory(ISerializer<byte[]> serializer)
+        {
+            _serializer = serializer;
+        }
+
         public IMessage CreateMessage<TMessage>(TMessage message)
         {
             throw new NotImplementedException();
