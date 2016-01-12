05786979a59fb8bdd5228f9cbed47f9111d6781b
Deathlycraft <deathlycraft-github@deathlyenterprises.com>
2016-01-11 18:30:19 -0600

    Added DefaultServer and DefaultClient implementations.

---
+++ DeathlyEnterprises.Networking/Default Implementations/DefaultClient.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DeathlyEnterprises.Networking
+{
+    public class DefaultClient : IClient
+    {
+        public void Connect(string ipAddress, int port)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DeathlyEnterprises.Networking/Default Implementations/DefaultServer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DeathlyEnterprises.Networking
+{
+    public class DefaultServer : IServer
+    {
+        public void Start(string ipAddress, int port)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
